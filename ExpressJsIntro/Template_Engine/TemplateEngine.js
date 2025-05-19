e607528666f653539f328683443aeaf8b2b270bb
Raj <rajsingh704596@gmail.com>
2025-05-19 19:25:21 +0530

    EJS Partial in express.js: Reusable file(like component of reactjs) using include and unescaped output , usecase- Header ejs file or footer ejs file render in other ejs file

--- ExpressJsIntro/Template_Engine/TemplateEngine.js
+++ ExpressJsIntro/Template_Engine/TemplateEngine.js
@@ -10,7 +10,7 @@
 //* e.g       res.render('index',{title:'Welcome', message:'Hello World'});
 
 
-//$ EJS -
+//$ EJS -       {it's work like jsx (where inside javascript html code use and inside html curly braces use for dynamic js data render) but on the Other hand EJS - Inside HTML file we render Js Data using ejs <% _js_ %> for print <%= variable %> }
 //* Install the engine using npm(e.g, npm install ejs) and create templates with placeholder like  <%= title %> for data insertion.
 //# EJS(Embedded JavaScript) is a simple templating engine for generating dynamic HTML pages.
 //# It integrates JavaScript logic into HTML using special tags like <%=  %> 
@@ -18,13 +18,13 @@
 //# Outputs the value into the template Escapes HTML (<%= %>) by default to prevent injection, with options for unescaped output (<%-).
 //# EJS Docs so many tags available
 
-//^ Ejs syntax - for Comments :-  
+//^1 Ejs syntax - for Comments :-  
 //* Add comments in your templates using  <%#  ...  %> , These are not included in the output.
    // <%# This is a comment %>
    // <p> Visible content </p>
 
 
-//^ Ejs Syntax - Outputting Values
+//^2 Ejs Syntax - Outputting Values
 
 //* Escaped Output (<%= ... %>) 
 //  Used to print dynamic content, escaping any special HTML characters to prevent injection.
@@ -50,7 +50,7 @@
     // <div><strong>Bold Text</strong></div>
 
 
- //^ Ejs Syntax- Loops
+ //^3 Ejs Syntax- Loops
  // use Javascript for loop / foreach loops to render lists. Note Every line opening or closing ejs tag <% %> use
   
     //^ Ejs in html file with for loop 
@@ -88,3 +88,14 @@
     //     <li> banana </li>
     //     <li> cherry </li>
     // </ul>
+
+
+//^ 4. EJs Syntax-  Include partials        {it's work like component for re-use }
+//* Re-use common templates " using include" like header, footers, etc.     
+
+  // ejs html file
+  //  <%- include('header') %>              <!-- unescape output use for header component show -->
+  //  <h1>Welcome to my site</h1>
+  //  <%- include('footer') %>
+//^ make sure header.ejs and footer.ejs are in your views folder
+//^ if they are inside a folder then use folder/header    
